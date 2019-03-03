4cd18e9d602b76b44c58f0e45a4fc55c87f5caf4
Alina Pospelova <pospelowa.alina@yandex.ru>
2019-03-03 17:32:09 +0300

    Update SimpleLRU.cpp
    Added "Set".

--- src/storage/SimpleLRU.cpp
+++ src/storage/SimpleLRU.cpp
@@ -13,7 +13,12 @@ bool SimpleLRU::Put(const std::string &key, const std::string &value) {
 bool SimpleLRU::PutIfAbsent(const std::string &key, const std::string &value) { return false; }
 
 // See MapBasedGlobalLockImpl.h
-bool SimpleLRU::Set(const std::string &key, const std::string &value) { return false; }
+bool SimpleLRU::Set(const std::string &key, const std::string &value) {
+	if (_lru_index.find(key) == _lru_index.end()) return false; // There is not such a key.
+	if ((key.size() + value.size()) > _max_size) return false; // This pair does not fit in the cache.
+	Delete(key); // Delete the obsolete pair with this key.
+	return PutIfAbsent(key, value); // Move new item with this key and value on the top.
+}
 
 // See MapBasedGlobalLockImpl.h
 bool SimpleLRU::Delete(const std::string &key) { return false; }

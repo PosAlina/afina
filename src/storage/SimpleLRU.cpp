499b0635e52d3329449fb9125302b401d755e36c
Alina Pospelova <pospelowa.alina@yandex.ru>
2019-03-03 17:27:34 +0300

    Update SimpleLRU.cpp
    Added "Put" and "Get"

--- src/storage/SimpleLRU.cpp
+++ src/storage/SimpleLRU.cpp
@@ -4,7 +4,10 @@ namespace Afina {
 namespace Backend {
 
 // See MapBasedGlobalLockImpl.h
-bool SimpleLRU::Put(const std::string &key, const std::string &value) { return false; }
+bool SimpleLRU::Put(const std::string &key, const std::string &value) {
+	if (_lru_index.find(key) != _lru_index.end()) return Set(key, value); // There is already such a key.
+	return PutIfAbsent(key, value);	// There is not such a key.
+}
 
 // See MapBasedGlobalLockImpl.h
 bool SimpleLRU::PutIfAbsent(const std::string &key, const std::string &value) { return false; }
@@ -16,7 +19,13 @@ bool SimpleLRU::Set(const std::string &key, const std::string &value) { return f
 bool SimpleLRU::Delete(const std::string &key) { return false; }
 
 // See MapBasedGlobalLockImpl.h
-bool SimpleLRU::Get(const std::string &key, std::string &value) const { return false; }
+// Do not need "const", as it is necessary to renew the popularity of an item.
+bool SimpleLRU::Get(const std::string &key, std::string &value) { // const
+	auto need_iterator = _lru_index.find(key);
+	if (need_iterator == _lru_index.end()) return false; // There is not such a key.
+	value = need_iterator->second.get().value; // There is such an item.
+	return Set(key, value);	// Move this item on the top.
+}
 
 } // namespace Backend
 } // namespace Afina

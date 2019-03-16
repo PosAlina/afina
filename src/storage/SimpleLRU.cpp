f6470f4a0a0b8637a1e2d33a8d91f7fbd99babe4
Alina Pospelova <pospelowa.alina@yandex.ru>
2019-03-16 08:35:16 +0300

    Update SimpleLRU.cpp
    HW1

--- src/storage/SimpleLRU.cpp
+++ src/storage/SimpleLRU.cpp
@@ -8,8 +8,6 @@ bool SimpleLRU::PutNewNode(const std::string &key, const std::string &value) {
 	// Delete obsolete fields until there is free space.
 	while (_storage_size + key.size() + value.size() > _max_size)
 		Delete(_lru_last_node->key);
-	// Input the new node in the index storage.
-	_lru_index.insert({std::reference_wrapper<const std::string>(current_node->key), std::reference_wrapper<lru_node>(*current_node)});
 	_storage_size += key.size() + value.size();
 	// Input the new node in a head
 	lru_node *current_node;
@@ -23,6 +21,8 @@ bool SimpleLRU::PutNewNode(const std::string &key, const std::string &value) {
 		_lru_head.reset(current_node);
 		_lru_last_node = current_node;
 	}
+	// Input the new node in the index storage.
+	_lru_index.insert({std::reference_wrapper<const std::string>(current_node->key), std::reference_wrapper<lru_node>(*current_node)});
 	return true;
 }
 
@@ -35,7 +35,7 @@ bool SimpleLRU::UpdateNode(const std::string &key, const std::string &value) {
 // See MapBasedGlobalLockImpl.h
 bool SimpleLRU::Put(const std::string &key, const std::string &value) {
 	if ((key.size() + value.size()) > _max_size) return false; // This pair does not fit in the cache.	
-	if (_lru_index.find(key) != _lru_index.end()) return UpdateField(key, value); // There is already such a key.
+	if (_lru_index.find(key) != _lru_index.end()) return UpdateNode(key, value); // There is already such a key.
 	return PutNewNode(key, value);	// There is not such a key.
 }
 
@@ -61,7 +61,7 @@ bool SimpleLRU::Delete(const std::string &key) {
 	_lru_index.erase(key);
 	_storage_size -= (key.size() + need_iterator->second.get().value.size());
 	// Delete the pair from the head storage.
-	lru_head *current_node = _lru_head.get();
+	lru_node *current_node = _lru_head.get();
 	// Search need node.
 	while(current_node != nullptr) {
 		if (current_node->key == key)
@@ -70,8 +70,8 @@ bool SimpleLRU::Delete(const std::string &key) {
 	}
 	if (current_node->next != nullptr) { // It is not the last node.
 		//Change prev node.
-		if (current_node->prev == nullptr) head = std::move(current_node->next); // It is first node.
-		else current_node->prev->next = std::move(currrent_node->next); //It is not first node.
+		if (current_node->prev == nullptr) _lru_head = std::move(current_node->next); // It is first node.
+		else current_node->prev->next = std::move(current_node->next); //It is not first node.
 		current_node->next->prev = current_node->prev;
 	}
 	else { // It is the last node.
